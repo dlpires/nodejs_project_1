a2608981dc72df9608a46653a92c3c741838346d
Diego Luis Pires <diegoluispires@gmail.com>
2023-04-13 22:22:43 -0300

    Aula 01 - Node + EJS + Bootstrap

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+//ESTOU DIZENDO PARA DIZER QUE O EXPRESS TERÁ O RENDERIZADOR EJS
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get("/",(req,res) => {
+    res.render("index");
+});
+
+app.get("/perguntar",(req,res) => {
+    res.render("perguntar");
+});
+
+app.listen(8080,()=>{
+    console.log("App rodando!")
+});
