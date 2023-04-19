bcd88623273fb00826d2b50fd07201dada9e13a2
Diego Luis Pires <diegoluispires@gmail.com>
2023-04-18 23:41:07 -0300

    Update database.js

--- database/database.js
+++ database/database.js
@@ -8,8 +8,14 @@ const DB_HOST = "localhost"
 
 const connection = new Sequelize(DATABASE,DB_USER,DB_PASSWORD,{
     host: DB_HOST,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    /*dialectOptions: {
+        socketPath: '/var/run/mysqld/mysqld.sock'
+    }*/ // NECESSÁRIO PARA DEPLOY EM LINUX (DIGITAL OCEAN)
 });
 
 module.exports = connection;
 //user: nodeproject1, senha: N0d3J52320@
+//ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'abc@123';
+//INSTALL PM2 (NODEJS APP EM BACKGROUND) -> npm install -g pm2
+//Start -> pm2 start index.js
